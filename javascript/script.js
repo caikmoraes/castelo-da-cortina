33f5ffd7804b38634b4fddac3e175256748be8de
Caik Moraes <caikdemoraes@gmail.com>
2020-06-16 18:28:44 -0300

    Segundo commit

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+// Menu function
+$(function() {
+    $(".toggle").on("click", function(){
+        if($(".item").hasClass("active")) {
+            $(".item").removeClass("active")
+        }
+        else{
+            $(".item").addClass("active")
+        }
+    })
+})
