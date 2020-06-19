5f0dfaa0a31f4fd2f66952dc8b05f096babb5375
Caik Moraes <caikdemoraes@gmail.com>
2020-06-19 12:00:44 -0300

    Inserção de conteúdo na pagina inicial

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,20 @@
 // Menu function
 $(function() {
-    $(".toggle").on("click", function(){
-        if($(".item").hasClass("active")) {
+    $(".toggle").on("click", function() {
+        if ($(".item").hasClass("active")) {
             $(".item").removeClass("active")
-        }
-        else{
+        } else {
             $(".item").addClass("active")
         }
     })
 })
+
+function openMenu() {
+    document.getElementById('open-menu').style.display = 'none'
+    document.getElementById('exit-menu').style.display = 'block'
+}
+
+function exitMenu() {
+    document.getElementById('open-menu').style.display = 'block'
+    document.getElementById('exit-menu').style.display = 'none'
+}
