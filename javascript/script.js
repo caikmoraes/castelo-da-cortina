249037fc7754229a37e290d6841f6ea51ccedd81
ThiagoSR27 <thiagof1212@gmail.com>
2020-06-21 14:01:45 -0300

    adição do botao de retornar ao topo

--- javascript/script.js
+++ javascript/script.js
@@ -18,3 +18,21 @@ function exitMenu() {
     document.getElementById('open-menu').style.display = 'block'
     document.getElementById('exit-menu').style.display = 'none'
 }
+
+//botao de voltar ao topo
+window.onscroll = function(){
+    scroll();
+}
+function scroll(){
+    var btn =document.getElementById("btnTop");
+    if(document.documentElement.scrollTop > 50){
+        btn.style.display="block"
+    }
+    else{
+        btn.style.display="none"
+    }
+}
+
+function voltarTopo(){
+    document.documentElement.scrollTop=0;
+}
