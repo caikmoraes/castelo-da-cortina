c40c5c806b147823fe9f7b572b1c59de3b652b25
Caik Moraes <caikdemoraes@gmail.com>
2020-06-24 13:34:42 -0300

    Teste de galeria infinita

--- javascript/script.js
+++ javascript/script.js
@@ -39,6 +39,27 @@ function voltarTopo() {
 }
 
 // Gallery function
+window.onload = () => setGallery()
+
+function setGallery() {
+    document.addEventListener('DOMContentLoaded', function() {
+        var stream = document.querySelector('.gallery__stream');
+        var items = document.querySelectorAll('.gallery__item');
+
+        var prev = document.querySelector('.gallery__prev');
+        prev.addEventListener('click', function() {
+            stream.insertBefore(items[items.length - 1], items[0]);
+            items = document.querySelectorAll('.gallery__item');
+        });
+
+        var next = document.querySelector('.gallery__next');
+        next.addEventListener('click', function() {
+            stream.appendChild(items[0]);
+            items = document.querySelectorAll('.gallery__item');
+        });
+    });
+}
+
 function showFullGallery() {
     let scrollPosition = [
         self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
