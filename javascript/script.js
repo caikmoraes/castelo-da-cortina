9ce0f528b0add2a1c8aa3c2f970826ec6e702b1f
Caik Moraes <caikdemoraes@gmail.com>
2020-06-24 11:38:39 -0300

    Inserção de galeria de fotos

--- javascript/script.js
+++ javascript/script.js
@@ -37,3 +37,32 @@ function scroll() {
 function voltarTopo() {
     document.documentElement.scrollTop = 0;
 }
+
+// Gallery function
+function showFullGallery() {
+    let scrollPosition = [
+        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+        self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+    ];
+    let html = jQuery('html');
+    html.data('scroll-position', scrollPosition);
+    html.data('previous-overflow', html.css('overflow'));
+    html.css('overflow', 'hidden');
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+    let btnTop = document.getElementById('btnTop')
+    let gallery = document.getElementById('full-gallery')
+    gallery.style.left = "0%";
+    btnTop.style.display = 'none'
+
+}
+
+function exitGallery() {
+    let html = jQuery('html');
+    let scrollPosition = html.data('scroll-position');
+    html.css('overflow', html.data('previous-overflow'));
+    window.scrollTo(scrollPosition[0], scrollPosition[1])
+    let btnTop = document.getElementById('btnTop')
+    let gallery = document.getElementById('full-gallery')
+    gallery.style.left = "100%";
+    btnTop.style.display = 'block'
+}
