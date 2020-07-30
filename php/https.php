3f0e8c13fcdc235528dc0e19b7aa5bbf411250a2
caikmoraes <caikdemoraes@gmail.com>
2020-07-30 06:51:21 -0300

    Correção de css de galerias e fonte

--- php/https.php
+++
@@ -1,8 +0,0 @@
-<?php 
-// Require https
-if ($_SERVER['HTTPS'] != "on") {
-    $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-    header("Location: $url");
-    exit;
-}
-?>
